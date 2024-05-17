55bfbc8891f0b9d73f9d432fe9350a759bdb2574
uddeshyac9 <uddeshyac9@gmail.com>
2024-05-17 15:16:01 +0530

    updated

---
+++ app/(tabs)/SearchRecipe.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function SearchRecipe() {
+  return (
+    <View >
+      <Text>SearchRecipe</Text>
+    </View>
+  )
+}
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,9 +24,9 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="SearchRecipe"
         options={{
-          title: 'Explore',
+          title: 'Search Recipe',
           tabBarIcon: ({ color, focused }) => (
             <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
           ),
